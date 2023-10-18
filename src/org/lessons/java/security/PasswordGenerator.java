e7d550ee2d4293c116bb50f703cb73cd2f5fd6c7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 16:43:59 +0200

    add variable name

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,14 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+        //inizializzare lo scan
+        Scanner scan = new Scanner(System.in);
 
+        //Chiedere il nome
+        System.out.println("Qual'è il tuo nome?");
+        String name = scan.nextLine();
     }
 }
