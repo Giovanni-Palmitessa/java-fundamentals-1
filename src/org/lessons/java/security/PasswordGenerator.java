3530a1424d9e7e18520898b14a5d9ea594d173d8
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 16:45:57 +0200

    add variable surname,color,birthday

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -10,5 +10,17 @@ public class PasswordGenerator {
         //Chiedere il nome
         System.out.println("Qual'è il tuo nome?");
         String name = scan.nextLine();
+
+        //Chiedere il cognome
+        System.out.println("Qual'è il tuo cognome?");
+        String surname = scan.nextLine();
+
+        //Chiedere il colore preferito
+        System.out.println("Qual'è il tuo colore preferito?");
+        String color = scan.nextLine();
+
+        //Chiedere data di nascita
+        System.out.println("Qual'è la tua data di nascita (Formato DD-MM-AAAA)?");
+        String birthday = scan.nextLine();
     }
 }
