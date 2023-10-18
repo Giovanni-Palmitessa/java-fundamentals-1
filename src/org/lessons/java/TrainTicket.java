47f0594cf60ece58a4dc360cbfd7402d2a53f475
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 15:39:04 +0200

    add variables kmtotravel and age

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -6,5 +6,13 @@ public class TrainTicket {
     public static void main(String[] args) {
         //inizializzare lo scan
         Scanner scan = new Scanner(System.in);
+
+        //Chiedere quanti chilometri si vogliono percorrere
+        System.out.println("Quanti km vuoi percorrere?");
+        int kmToTravel = scan.nextInt();
+
+        //Chiedere età passeggero
+        System.out.println("Quanti anni hai?");
+        int age = scan.nextInt();
     }
 }
