972db140bc1261de6622bf38afcd6e7448d732dd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 14:51:58 +0200

    Complete first exercise

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -2,8 +2,17 @@ package org.lessons.java;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        boolean fizz = false;
-        boolean buzz = false;
-        boolean fizzBuzz = false;
+
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz " + "numero: " + i + ")");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz " + "(numero: " + i + ")");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz "+ "(numero: " + i + ")");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
