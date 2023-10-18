ef4e8a40c13874d9f6f4c370183742699052fbe0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 15:34:39 +0200

    initialize scan

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,7 +1,10 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class TrainTicket {
     public static void main(String[] args) {
-
+        //inizializzare lo scan
+        Scanner scan = new Scanner(System.in);
     }
 }
