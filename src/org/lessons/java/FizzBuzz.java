454e116abaeeeed39e8924920a90965cda88632e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-18 14:25:29 +0200

    Add boolean variables

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,4 +1,9 @@
 package org.lessons.java;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        boolean fizz = false;
+        boolean buzz = false;
+        boolean fizzBuzz = false;
+    }
 }
